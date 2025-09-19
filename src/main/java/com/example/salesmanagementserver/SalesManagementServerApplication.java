1a0f85acf87b723f287fef945a1a3d429a7bf30c
Hoang Tu <thocodeonline@gmail.com>
2025-09-19 16:57:51 +0700

    Thử push lại

---
+++ src/main/java/com/example/salesmanagementserver/SalesManagementServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.salesmanagementserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesManagementServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesManagementServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/salesmanagementserver/SalesManagementServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.salesmanagementserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesManagementServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
